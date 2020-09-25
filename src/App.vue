838f0e70aeee5f03cbb8a50397d14913297b7dcc
Mohammed Aarif <ali.aliaarif@gmail.com>
2020-09-25 16:45:42 +0530

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,62 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-navigation-drawer app v-if="drawer">
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#970747" dark>
+      <v-app-bar-nav-icon to="/"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>UNET WORLD</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="mr-n4">
+        <v-btn to="/signup" text>Sign Up</v-btn>
+        <v-btn to="/login" text>Log In</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view />
+      </v-container>
+    </v-main>
+
+    <v-footer app color="#970747" dark fixed>
+      <v-spacer></v-spacer>
+      <div>Unet World &copy; {{ new Date().getFullYear() }}</div>
+      <v-spacer></v-spacer>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+//import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    //HelloWorld,
+  },
+
+  data: () => ({
+    drawer: false,
+  }),
+};
+</script>
+<style>
+h1 {
+  color: #970747;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+p {
+  color: #970747;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,20 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('../views/Signup.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
